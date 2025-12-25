6552f9c362cda37b10568a9c4840db11360f58f8
Ever-VC <ever.vasquezc27@gmail.com>
2025-12-24 21:03:48 -0600

    :sparkles: feat(Product service): Obteniendo los datos de la capa de persistencia y modificando segun se necesiten con el service de producto

--- src/main/java/com/evervc/springboot/di/app/springbootdi/services/ProductService.java
+++ src/main/java/com/evervc/springboot/di/app/springbootdi/services/ProductService.java
@@ -1,4 +1,24 @@
 package com.evervc.springboot.di.app.springbootdi.services;
 
+import com.evervc.springboot.di.app.springbootdi.models.Product;
+import com.evervc.springboot.di.app.springbootdi.repositories.ProductRepository;
+
+import java.util.List;
+
+// Esta es la capa de negocios, donde se pueden modificar los datos obtenidos del repositorio, según se necesiten
 public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll() {
+        // Modificando el valor del precio para todos los objetos
+        return repository.findAll().stream().map(product -> {
+            product.setPrice((long) (product.getPrice() * 1.25));
+            return product;
+        }).toList();
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id);
+    }
 }
