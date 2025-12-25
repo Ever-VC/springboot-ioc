afd72381f0ccbfe43d379dcaa9cf02dbb030b4ad
Ever-VC <ever.vasquezc27@gmail.com>
2025-12-24 21:29:48 -0600

    :sparkles: feat(ProductController): Rest Controller para gestion de datos

---
+++ src/main/java/com/evervc/springboot/di/app/springbootdi/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.evervc.springboot.di.app.springbootdi.controllers;
+
+import com.evervc.springboot.di.app.springbootdi.models.Product;
+import com.evervc.springboot.di.app.springbootdi.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductController {
+
+    private ProductService service = new ProductService();
+
+    @GetMapping
+    public List<Product> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+}
